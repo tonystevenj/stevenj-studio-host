db2b726aac917198e2106580df654940e9f57f45
Steven Wang <tonystevenj@gmail.com>
2021-03-22 01:13:15 -0400

    add pwa feature

---
+++ precache-manifest.f15f4a2ef6bfe8755fd1b96217632067.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38100f3973d7acae00b",
+    "url": "css/app.9b1aface.css"
+  },
+  {
+    "revision": "d85adae6739c0f72a23c2ab21976a8c2",
+    "url": "img/close.d85adae6.svg"
+  },
+  {
+    "revision": "7d97c59fed369a920919334382def52a",
+    "url": "img/icons8-facebook.7d97c59f.svg"
+  },
+  {
+    "revision": "c500b4caab84c8877b55af20de423fdb",
+    "url": "img/icons8-github.c500b4ca.svg"
+  },
+  {
+    "revision": "91f84717607ee861577f98d1090f4c8b",
+    "url": "img/icons8-gmail.91f84717.svg"
+  },
+  {
+    "revision": "b5b2fe197fab34ac80df27da5aff5a26",
+    "url": "img/icons8-instagram.b5b2fe19.svg"
+  },
+  {
+    "revision": "ee926207b5d5ea7d3ce44dac58f47732",
+    "url": "img/icons8-linkedin-circled.ee926207.svg"
+  },
+  {
+    "revision": "a293880d9cb1ff1deae81c248815c571",
+    "url": "img/menu-white.a293880d.svg"
+  },
+  {
+    "revision": "0d3fa5f07bdf142214ae82b64fd3012e",
+    "url": "img/menu.0d3fa5f0.svg"
+  },
+  {
+    "revision": "60d0fa29301998a758f515098c0061a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "e38100f3973d7acae00b",
+    "url": "js/app.b8b629d8.js"
+  },
+  {
+    "revision": "60d15316433fcf716b29",
+    "url": "js/chunk-vendors.db6146f5.js"
+  },
+  {
+    "revision": "9666889891c8eb187342929f7e2e9537",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8d5c54a5d52e241e0591cebe8dee8c2f",
+    "url": "s-256.png"
+  },
+  {
+    "revision": "68d15f2deb3cece25e9771dc0ac8a751",
+    "url": "s-svg.svg"
+  },
+  {
+    "revision": "78cb32ba9e454711f499158f50050557",
+    "url": "s.png"
+  },
+  {
+    "revision": "5ceec7ff81198d7a214f84263eae6e92",
+    "url": "t.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.f15f4a2ef6bfe8755fd1b96217632067.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-studio"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
